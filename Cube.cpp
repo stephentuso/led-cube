0b86e8b1bb1238b2ed91709c686d6f89c8f662da
Stephen Tuso <stephentuso@gmail.com>
2016-03-29 19:30:30 +0000

    Cube.cpp edited online with Bitbucket

--- Cube.cpp
+++ Cube.cpp
@@ -499,6 +499,13 @@ void Cube::printCharacter(char character, String dir, int _speed){
       int4 = 0b00000;
       int5 = 0b00000;
       break;
+    case '%':
+      int1 = 0b10011;
+      int2 = 0b01011;
+      int3 = 0b00100;
+      int4 = 0b11010;
+      int5 = 0b11001;
+      break;
     case '\370':
       int1 = 0b11000;
       int2 = 0b11000;
